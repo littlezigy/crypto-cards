872f293f72da68a93ab298d947555f33195fb2ba
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-07-25 07:13:25 +0100

    Added chat

--- DApp/Server/bin/www.js
+++ DApp/Server/bin/www.js
@@ -1,8 +1,14 @@
+const url = require('url');
+const http = require('http');
+const server = http.createServer();
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 3080 });
+const app = require('../app');
 
-wss.on('connection', function connection(ws) {
+const wsChat = new WebSocket.Server({ noServer: true });
+const ws1 = new WebSocket.Server({ noServer: true });
+
+ws1.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
         ws.send("Message received");
@@ -10,3 +16,34 @@ wss.on('connection', function connection(ws) {
     ws.send('something');
 });
 
+wsChat.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        wsChat.clients.forEach((client) => {
+            if(client.readyState === WebSocket.OPEN
+                && client !== ws
+            ) {
+                client.send(message)
+            }
+        });
+    });
+    ws.send('Chat');
+});
+
+server.on('upgrade', (request, socket, head) => {
+    const route = url.parse(request.url).pathname;
+    // console.log('ROUTE', route);
+
+    if(route === '/chat') {
+        wsChat.handleUpgrade(request, socket, head, function done(ws) {
+            wsChat.emit('connection', ws, request);
+        });
+    } else if(route === '/boon') {
+        ws1.handleUpgrade(request, socket, head, (ws) => {
+            ws1.emit('connection', ws, request);
+        });
+    } else {
+        socket.destroy();
+    }
+});
+
+server.listen(3080, 'localhost');
