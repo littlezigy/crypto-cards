9e0c7e63a3151918626ed63e041c0ee257f7916a
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-07-26 23:49:07 +0100

    Fixed references to assets

--- DApp/Server/app.js
+++ DApp/Server/app.js
@@ -4,12 +4,18 @@ const path = require('path');
 
 const router = require('./config/routes');
 
-let pathy = path.join(__dirname, '../Client/assets');
+// let pathy = path.join(__dirname, '../Client/assets');
+let css = path.join(__dirname, '../Client/css');
+let img = path.join(__dirname, '../Client/img');
+let js = path.join(__dirname, '../Client/js');
 
-console.log('APTHY', pathy);
+// console.log('APTHY', pathy);
 
 app.use(router);
-app.use('/', express.static(pathy));
+// app.use('/', express.static(pathy));
+app.use('/css', express.static(css));
+app.use('/js', express.static(js));
+app.use('/img', express.static(img));
 
 
 module.exports = { app };
