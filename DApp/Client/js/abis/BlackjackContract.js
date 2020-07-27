a2d7590ae8f20367b007dbb7ad1741f3053a2ada
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-07-27 23:54:08 +0100

    Abis

---
+++ DApp/Client/js/abis/BlackjackContract.js
@@ -0,0 +1,160 @@
+var blackjackAbi =
+    [
+  {
+    inputs: [ [Object] ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+    constant: undefined,
+    payable: undefined
+  },
+  {
+    anonymous: false,
+    inputs: [ [Object] ],
+    name: 'FillingDeck',
+    type: 'event',
+    constant: undefined,
+    payable: undefined,
+    signature: '0x280a9bbf14a74773ae5f7fbebddd46d5feef32ab6b1930f1bee5496f76c15235'
+  },
+  {
+    inputs: [ [Object] ],
+    name: 'deck',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0x65b1dafa'
+  },
+  {
+    inputs: [ [Object] ],
+    name: 'setPlayerName',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0xd8177369'
+  },
+  {
+    inputs: [ [Object] ],
+    name: 'setRank',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0x8557e1ee'
+  },
+  {
+    inputs: [ [Object] ],
+    name: 'setSuit',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0xb61da41f'
+  },
+  {
+    inputs: [ [Object] ],
+    name: 'setBet',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+    payable: true,
+    constant: undefined,
+    signature: '0xe8399cc6'
+  },
+  {
+    inputs: [ [Object] ],
+    name: 'setPot',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0x80b5b80c'
+  },
+  {
+    inputs: [],
+    name: 'getPlayerName',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0xa69c6597'
+  },
+  {
+    inputs: [],
+    name: 'getRank',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0xd303e7b2'
+  },
+  {
+    inputs: [],
+    name: 'getSuit',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0xf9732960'
+  },
+  {
+    inputs: [],
+    name: 'getBet',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0x20835e8c'
+  },
+  {
+    inputs: [],
+    name: 'getBlackjackPlayer',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0xe22e5492'
+  },
+  {
+    inputs: [],
+    name: 'shuffleDeck',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0xb0a3beaf'
+  },
+  {
+    inputs: [],
+    name: 'calcPoints',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0xf6fc28c9'
+  },
+  {
+    inputs: [],
+    name: 'getDeck',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0x33943474'
+  }
+]
+
---
+++ DApp/Client/js/abis/chipVRFabi.js
@@ -0,0 +1,112 @@
+let chipVRFabi = 
+    [
+  {
+    anonymous: false,
+    inputs: [ [Object], [Object], [Object] ],
+    name: 'Approval',
+    type: 'event',
+    constant: undefined,
+    payable: undefined,
+    signature: '0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925'
+  },
+  {
+    anonymous: false,
+    inputs: [ [Object], [Object], [Object] ],
+    name: 'Transfer',
+    type: 'event',
+    constant: undefined,
+    payable: undefined,
+    signature: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+  },
+  {
+    inputs: [ [Object], [Object] ],
+    name: 'allowance',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0xdd62ed3e'
+  },
+  {
+    inputs: [ [Object] ],
+    name: 'balanceOf',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0x70a08231'
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0x06fdde03'
+  },
+  {
+    inputs: [],
+    name: 'symbol',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0x95d89b41'
+  },
+  {
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [ [Object] ],
+    stateMutability: 'view',
+    type: 'function',
+    constant: true,
+    payable: undefined,
+    signature: '0x18160ddd'
+  },
+  {
+    inputs: [ [Object] ],
+    name: 'cryptoCardsChip',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0x68234198'
+  },
+  {
+    inputs: [ [Object], [Object] ],
+    name: 'transfer',
+    outputs: [ [Object] ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0xa9059cbb'
+  },
+  {
+    inputs: [ [Object], [Object] ],
+    name: 'approve',
+    outputs: [ [Object] ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0x095ea7b3'
+  },
+  {
+    inputs: [ [Object], [Object], [Object] ],
+    name: 'transferFrom',
+    outputs: [ [Object] ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+    constant: undefined,
+    payable: undefined,
+    signature: '0x23b872dd'
+  }
+]
+
