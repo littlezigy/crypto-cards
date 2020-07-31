a3a7529ecaff573c91c9bf1a79266afac761a956
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-07-31 14:56:41 +0100

    Updated migrations

--- DApp/migrations/3_cardgames_migration.js
+++ DApp/migrations/3_cardgames_migration.js
@@ -1,5 +1,11 @@
 const BlackJack = artifacts.require('Blackjack');
+const VRFChainlink = artifacts.require('RandomNumberConsumer');
 
 module.exports = function(deployer) {
-    deployer.deploy(BlackJack);
+    // deployer.deploy(BlackJack);
+//    deployer.deploy(VRFChainlink);
+    deployer.deploy(VRFChainlink)
+    .then(() => {
+        return deployer.deploy(BlackJack, VRFChainlink.address);
+    });
 }
--- DApp/migrations/4_chainlink_vrf_migration.js
+++
@@ -1,5 +0,0 @@
-const VRFChainlink = artifacts.require('VRFTestnetD20');
-
-module.exports = function(deployer) {
-    deployer.deploy(VRFChainlink, '0xf720CF1B963e0e7bE9F58fd471EFa67e7bF00cfb', '0x20fE562d797A42Dcb3399062AE9546cd06f63280');
-}
